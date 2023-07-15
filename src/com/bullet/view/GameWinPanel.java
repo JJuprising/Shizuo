4a85a20b62f56f56c2c27aed2440b10c77907895
Muya-123 <ray914173732@outlook.com>
2023-07-16 01:11:33 +0800

    结算界面标签

--- src/com/bullet/view/GameWinPanel.java
+++ src/com/bullet/view/GameWinPanel.java
@@ -12,28 +12,27 @@ import java.awt.event.ActionListener;
 
 public class GameWinPanel extends JPanel {
     public GameWinPanel(){
-    	ImageIcon background;
-        background = new ImageIcon("res/images/background/LOGO.png");
+
     	ImageIcon start = new ImageIcon("res/images/background/Start.png");
     	ImageIcon exit = new ImageIcon("res/images/background/Exit.png");
     	
-    	background.setImage(background.getImage().getScaledInstance(Settings.GameX,Settings.GameY + Settings.GameInfoY,Image.SCALE_DEFAULT));
     	start.setImage(start.getImage().getScaledInstance(200,50,Image.SCALE_DEFAULT));
     	exit.setImage(exit.getImage().getScaledInstance(200,50,Image.SCALE_DEFAULT));
     	
-        JLabel label1 = new JLabel(background);
+        Label label1 = new Label();
         Label label2 = new Label();
-        Label label3 = new Label();
 
-        GameManager.getManager().SetLabel2(label2,label3);
+        GameManager.getManager().SetLabel2(label1,label2);
         setLayout(null);
 
         // 创建两个按钮
         JButton button1 = new JButton(start);
         JButton button2 = new JButton(exit);
 
-        label1.setBounds(0,0,Settings.GameX,Settings.GameY + Settings.GameInfoY);
+        label1.setBounds(200,0,300,300);
         label1.setFont(new Font("宋体",Font.BOLD,20));
+        label2.setBounds(600,0,300,300);
+        label2.setFont(new Font("宋体",Font.BOLD,20));
         button1.setBounds(200,525,200,50);
         button2.setBounds(600,525,200,50);
 
@@ -57,7 +56,6 @@ public class GameWinPanel extends JPanel {
         this.add(button2);
         this.add(label1);
         this.add(label2);
-        this.add(label3);
 
         // 设置窗口大小和可见性
 //        setSize(400, 300);
